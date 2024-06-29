755b70826e7eda5b6d070d05e4814b081cca38c7
cyberneel <neelesh.chevuri25@gmail.com>
2024-06-29 16:09:27 +0530

    Added instagram icon to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,12 @@
+import { FaInstagram } from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <footer style={footerStyle}>
       <p>&copy; {new Date().getFullYear()} Dermalyze AI. All rights reserved.</p>
+      <a href="https://www.instagram.com/dermalyze" target="_blank" rel="noopener noreferrer" style={iconStyle}>
+        <FaInstagram />
+      </a>
     </footer>
   );
 };
@@ -16,6 +21,12 @@ const footerStyle = {
   backgroundColor: '#333',
   color: 'white',
   borderRadius: '15px',
+};
+
+const iconStyle = {
+  color: 'white',
+  marginLeft: '10px',
+  fontSize: '24px',
 };
 
 export default Footer;
