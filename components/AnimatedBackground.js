4b3a0cf5e1c6a75428605ccd23d9f2c7db747ab9
cyberneel <neelesh.chevuri25@gmail.com>
2024-06-30 23:44:07 +0530

    fixed background particles

--- components/AnimatedBackground.js
+++ components/AnimatedBackground.js
@@ -1,27 +1,32 @@
 // components/ParticlesBackground.js
 import React from 'react';
-import styles from './backgroundAnimation.module.css'; // Define your CSS styles
+import styles from './backgroundAnimation.module.css'; // Import CSS styles
 
 const ParticlesBackground = () => {
+  const particleCount = 20; // Adjust number of particles as needed
+
+  // Function to generate random positions for particles
+  const generateRandomPosition = () => {
+    const positions = [];
+    for (let i = 0; i < particleCount; i++) {
+      const style = {
+        left: `${Math.random() * 100}%`, // Random left position
+        top: `${Math.random() * 100}%`, // Random top position
+        animationDelay: `${Math.random() * 5}s`, // Random animation delay
+      };
+      positions.push(style);
+    }
+    return positions;
+  };
+
+  // Generate random positions for particles
+  const particlePositions = generateRandomPosition();
+
   return (
     <div className={styles.background}>
-      {/* Add multiple divs to create particles */}
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      <div className={styles.particle}></div>
-      {/* Add more particles as needed */}
+      {particlePositions.map((style, index) => (
+        <div key={index} className={styles.particle} style={style}></div>
+      ))}
     </div>
   );
 };
