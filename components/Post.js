7c971f0cd18672eed7d6485a32da53292b6f86ab
cyberneel <neelesh.chevuri25@gmail.com>
2024-07-08 10:11:59 +0530

    Updated posts and search components

--- components/Post.js
+++ components/Post.js
@@ -1,16 +1,23 @@
-function Post({ date, image, title }) {
-  let { file, description } = image
+// components/Post.js
 
+import Link from 'next/link';
+import styles from './Post.module.css';
+
+const Post = ({ post }) => {
   return (
-    <div className="post">
-      <img alt={description} src={`https:${file.url}`} />
-      <div className="description">{description}</div>
-      <div className="text">
-        <h2>{title}</h2>
-        <h3>{date.substring(0, 10)}</h3>
-      </div>
+    <div key={post.sys.id} className={styles.postBox}>
+      <Link href={`/posts/${post.fields.slug}`}>
+        <a>
+          {post.fields.imageUrl && (
+            <img src={post.fields.imageUrl} alt={post.fields.title} className={styles.postImage} />
+          )}
+          <h2 className={styles.postTitle}>{post.fields.title}</h2>
+          <p className={styles.postDate}>{new Date(post.fields.date).toLocaleDateString()}</p>
+          <p className={styles.postDescription}>{post.fields.description}</p>
+        </a>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
